33b0f5d7896723eefbe6da984d2fa6c7f8150d4d
devmatsu <devmatsu@hotmail.com>
2024-01-12 22:26:48 -0300

    Add eslint and prettier

--- 1-two-sum/index.js
+++ 1-two-sum/index.js
@@ -1,4 +1,4 @@
-export const twoSum = function(nums, target) {
+export const twoSum = function (nums, target) {
   const result = {};
 
   for (let i = 0; i < nums.length; i++) {
@@ -11,48 +11,46 @@ export const twoSum = function(nums, target) {
   return [];
 };
 
-export const twoSumV1 = function(nums, target) {
-  const result = []
+export const twoSumV1 = function (nums, target) {
+  const result = [];
   let nTargetSum = 0;
 
-  const arr = nums.map((elm, index) => {
-      return {
-          index: index,
-          item: elm,
-          used: false,
-      }
-  }).sort((a, b) => target > 0 ? b.item - a.item : a.item - b.item);
+  const arr = nums.map((elm, index) => ({
+    index,
+    item: elm,
+    used: false,
+  })).sort((a, b) => (target > 0 ? b.item - a.item : a.item - b.item));
 
   if (target !== 0) {
-      arr.forEach(elm => {
-          console.log(`item: ${elm.item} | targetSum: ${nTargetSum}`);
-          
-          if (!elm.used &&
-              (target > 0 && !((elm.item + nTargetSum) > target)) || 
-              (target < 0 && !((elm.item + nTargetSum) < target)) ) {
-              elm.used = true;
-              nTargetSum += elm.item;
-              result.push(elm.index);
-          }
-      })
-  } else {
-      const findExactValue = arr.filter(elm => elm.item === target);
-      if (findExactValue.length > 0) {
-          findExactValue.forEach(elm => {
-              result.push(elm.index);
-          }); 
-      } else {
-          arr.forEach(elm => {
-              const x = -(elm.item);
-              const findExactValue = arr.filter(elm => elm.item === x);
-              if (!elm.used && findExactValue.length > 0) {
-                  elm.used = true;
-                  findExactValue[0].used = true;
-                  result.push(elm.index, findExactValue[0].index);
-              }
-          })
+    arr.forEach((elm) => {
+      console.log(`item: ${elm.item} | targetSum: ${nTargetSum}`);
+
+      if (!elm.used
+              && (target > 0 && !((elm.item + nTargetSum) > target))
+              || (target < 0 && !((elm.item + nTargetSum) < target))) {
+        elm.used = true;
+        nTargetSum += elm.item;
+        result.push(elm.index);
       }
+    });
+  } else {
+    const findExactValue = arr.filter((elm) => elm.item === target);
+    if (findExactValue.length > 0) {
+      findExactValue.forEach((elm) => {
+        result.push(elm.index);
+      });
+    } else {
+      arr.forEach((elm) => {
+        const x = -(elm.item);
+        const findExactValue = arr.filter((elm) => elm.item === x);
+        if (!elm.used && findExactValue.length > 0) {
+          elm.used = true;
+          findExactValue[0].used = true;
+          result.push(elm.index, findExactValue[0].index);
+        }
+      });
+    }
   }
 
   return result.sort();
-}
+};
