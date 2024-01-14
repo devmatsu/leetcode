8fa058cf21924a848384ac0a20f91dc7075624b1
devmatsu <devmatsu@hotmail.com>
2024-01-14 15:30:37 -0300

    Add the solution of 26

--- 26-remove-duplicates-from-sorted-array/index.js
+++ 26-remove-duplicates-from-sorted-array/index.js
@@ -1,5 +1,14 @@
+/* eslint-disable no-param-reassign */
 export const removeDuplicates = (nums) => {
-  nums = [...new Set(nums)];
+  if (nums.length === 0) return 0;
 
-  return nums.length;
+  let uniqueIndex = 0;
+  for (let i = 1; i < nums.length; i += 1) {
+    if (nums[i] !== nums[uniqueIndex]) {
+      uniqueIndex += 1;
+      nums[uniqueIndex] = nums[i];
+    }
+  }
+
+  return uniqueIndex + 1;
 };
