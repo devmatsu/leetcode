fb98a8c262c2d4057abfb092a0eb0b739f5febb4
devmatsu <devmatsu@hotmail.com>
2024-02-03 21:56:31 -0300

    Change the folder organization and create exercises folder

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 const config = {
   testMatch: [
-    '<rootDir>/1-two-sum/index.test.js',
-    '<rootDir>/26-remove-duplicates-from-sorted-array/index.test.js',
-    '<rootDir>/41-first-missing-positive/index.test.js',
-    '<rootDir>/2667-create-hello-world-function/index.test.js',
+    '<rootDir>/exercises/1-two-sum/index.test.js',
+    '<rootDir>/exercises/26-remove-duplicates-from-sorted-array/index.test.js',
+    '<rootDir>/exercises/41-first-missing-positive/index.test.js',
+    '<rootDir>/exercises/2667-create-hello-world-function/index.test.js',
   ],
 };
 
