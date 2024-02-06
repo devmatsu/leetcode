3c1572a9ef391b223c6fbff789472b803fa622d2
devmatsu <devmatsu@hotmail.com>
2024-02-06 04:49:41 -0300

    Add 2634 solution

--- exercises/2634-filter-elements-from-array/index.js
+++ exercises/2634-filter-elements-from-array/index.js
@@ -3,6 +3,11 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var filter = function(arr, fn) {
-    
+
+export const filter = (arr, fn) => {
+  const result = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (fn(arr[i], i)) result.push(arr[i]);
+  }
+  return result;
 };
