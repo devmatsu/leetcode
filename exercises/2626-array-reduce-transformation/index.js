bbf564a9510b49501e3aa5a2ff8a4011bedb319b
devmatsu <devmatsu@hotmail.com>
2024-02-07 09:51:44 -0300

    Add 2626 solution

--- exercises/2626-array-reduce-transformation/index.js
+++ exercises/2626-array-reduce-transformation/index.js
@@ -4,6 +4,14 @@
  * @param {number} init
  * @return {number}
  */
-var reduce = function(nums, fn, init) {
-    
+
+export const reduce = (nums, fn, init) => {
+  if (nums.length === 0) return init;
+
+  let response = init;
+  for (let i = 0; i < nums.length; i += 1) {
+    response = fn(response, nums[i]);
+  }
+
+  return response;
 };
