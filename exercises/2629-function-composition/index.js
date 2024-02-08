dcc424db844067eb0f3141f2c18db95833884bc0
devmatsu <devmatsu@hotmail.com>
2024-02-08 10:10:41 -0300

    Add another 2629 solution

--- exercises/2629-function-composition/index.js
+++ exercises/2629-function-composition/index.js
@@ -4,6 +4,11 @@
  */
 
 export const compose = (functions) => (x) => {
+  if (functions.length === 0) return x;
+  return functions.reduceRight((acc, fn) => fn(acc), x);
+};
+
+export const composeV1 = (functions) => (x) => {
   let result = x;
   functions.reverse();
   functions.forEach((func) => {
