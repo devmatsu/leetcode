71a56324237a5dde7b89a1ff66e257f86ac1d056
devmatsu <devmatsu@hotmail.com>
2024-02-09 16:30:31 -0300

    add 2703 solution

--- exercises/2703-return-length-of-arguments-passed/index.js
+++ exercises/2703-return-length-of-arguments-passed/index.js
@@ -2,10 +2,5 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
-var argumentsLength = function(...args) {
-    
-};
 
-/**
- * argumentsLength(1, 2, 3); // 3
- */
+export const argumentsLength = (...args) => args.length;
