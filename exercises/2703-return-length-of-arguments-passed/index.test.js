2a239a7e9e119196655facea3f82240dba24882d
devmatsu <devmatsu@hotmail.com>
2024-02-09 16:50:01 -0300

    improve tests description

--- exercises/2703-return-length-of-arguments-passed/index.test.js
+++ exercises/2703-return-length-of-arguments-passed/index.test.js
@@ -1,17 +1,17 @@
 import { argumentsLength } from './index';
 
-describe('Return length of arguments', () => {
-  it('should return one argument', () => {
+describe('Counting the number of arguments', () => {
+  it('should return the count of one argument', () => {
     const args = [5];
     expect(argumentsLength(...args)).toEqual(1);
   });
 
-  it('should return five arguments', () => {
+  it('should return the count of five arguments', () => {
     const args = [5, 6, 7, 8, 9];
     expect(argumentsLength(...args)).toEqual(5);
   });
 
-  it('should return three arguments even with different types', () => {
+  it('should return the count of three arguments with different types', () => {
     const args = [{}, null, '3'];
     expect(argumentsLength(...args)).toEqual(3);
   });
