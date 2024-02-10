a5e320d3d0c5434a2a1468da5b6bb353abf81632
devmatsu <devmatsu@hotmail.com>
2024-02-10 19:58:37 -0300

    add 2666 solution

--- exercises/2666-allow-one-function-call/index.js
+++ exercises/2666-allow-one-function-call/index.js
@@ -2,17 +2,12 @@
  * @param {Function} fn
  * @return {Function}
  */
-var once = function(fn) {
-    
-  return function(...args){
-      
-  }
-};
 
-/**
-* let fn = (a,b,c) => (a + b + c)
-* let onceFn = once(fn)
-*
-* onceFn(1,2,3); // 6
-* onceFn(2,3,6); // returns undefined without calling fn
-*/
+export const once = (fn) => {
+  let flagOnce = false;
+  return (...args) => {
+    if (flagOnce) return undefined;
+    flagOnce = true;
+    return fn(...args);
+  };
+};
