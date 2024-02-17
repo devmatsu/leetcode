654707e0fe176c565db9f30a8a16ffa10ccbe3f6
devmatsu <devmatsu@hotmail.com>
2024-02-17 16:30:35 -0300

    add 2637 solution

--- exercises/2637-promise-time-limit/index.js
+++ exercises/2637-promise-time-limit/index.js
@@ -1,16 +1,20 @@
+/* eslint-disable no-async-promise-executor */
+/* eslint-disable prefer-promise-reject-errors */
 /**
  * @param {Function} fn
  * @param {number} t
  * @return {Function}
  */
-var timeLimit = function(fn, t) {
-    
-  return async function(...args) {
-      
-  }
-};
 
-/**
-* const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
-* limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
-*/
+export const timeLimit = (fn, t) => (...args) => new Promise(async (resolve, reject) => {
+  const id = setTimeout(() => reject('Time Limit Exceeded'), t);
+
+  try {
+    const res = await fn(...args);
+    resolve(res);
+  } catch (err) {
+    reject(err);
+  }
+
+  clearTimeout(id);
+});
